569e7bc5f1381c2b2dc35f2e94179f59f9de44f9
KentVUONG <namvuonghcm@gmail.com>
2013-08-13 10:08:49 +0700

    Update movie front end

--- app/Controller/MoviesController.php
+++ app/Controller/MoviesController.php
@@ -1,11 +1,28 @@
 <?php
 class MoviesController extends AppController {
+    
+    public $uses = array('Movie', 'MovieCategory');
 	
 	public function index(){
-		$videos = array();
-		$videos = array_merge($videos, array('KOSHIEN STADIUM AUGUST 1986' =>'//www.youtube.com/embed/Kyqnwkp1bWU'));
-		$videos = array_merge($videos, array('2013.4.20 Japanese Baseball Game in Koshien Stadium 2' =>'//www.youtube.com/embed/OHouzpumOGc'));
-		$videos = array_merge($videos, array('Crazy Pitcher Shinjo ' =>'//www.youtube.com/embed/WOxP8YQ1NYA'));
-		$this->set('videos', $videos);
+// 		$videos = array();
+// 		$videos = array_merge($videos, array('KOSHIEN STADIUM AUGUST 1986' =>'//www.youtube.com/embed/Kyqnwkp1bWU'));
+// 		$videos = array_merge($videos, array('2013.4.20 Japanese Baseball Game in Koshien Stadium 2' =>'//www.youtube.com/embed/OHouzpumOGc'));
+// 		$videos = array_merge($videos, array('Crazy Pitcher Shinjo ' =>'//www.youtube.com/embed/WOxP8YQ1NYA'));
+// 		$this->set('videos', $videos);
+
+	    
+	    $categories = $this -> MovieCategory -> getAll();
+	    $movies = $this -> Movie -> getAll();
+	    
+	    $catid = isset($_GET['moviecategory']) ? $_GET['moviecategory'] : '';
+	    if ($catid != '') {
+	      $movies = $this -> Movie -> getMovieByCategoryId ($catid);
+	    } else {
+	      $movies = $this -> Movie -> getAll();
+	    }
+	    
+	    
+	    $this -> set('movies', $movies);
+	    $this -> set('categories', $categories);
 	}
 }
